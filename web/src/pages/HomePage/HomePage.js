4b8ec152c38c3f5433a78a38209874766eaaa999
iambillmccann <bill.mccann@gmail.com>
2023-01-29 19:29:29 -0500

    Add chakra

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,4 +1,5 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Center, Box } from '@chakra-ui/react'
+
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
@@ -6,14 +7,9 @@ const HomePage = () => {
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <Box>
+        <Center>Bill McCann</Center>
+      </Box>
     </>
   )
 }
