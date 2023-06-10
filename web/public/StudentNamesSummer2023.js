49a0cdf79db0e8fd7b59419b4ae7486fc72b8fa1
iambillmccann <bill.mccann@gmail.com>
2023-06-10 18:09:52 -0400

    Add new students

---
+++ web/public/StudentNamesSummer2023.js
@@ -0,0 +1,27 @@
+const names = [
+  'Joseph Antonucci',
+  'Carlos D. Bonilla',
+  'Samridhya Mukherjee',
+  'Sean A Ramirez',
+  'Palina Pauliuchenka',
+  'Sean Reiche',
+  'Marwan Youssef',
+  'Faviola Bautista',
+  'Jacob Crews',
+  'Anthony A Ruano',
+  'Mikel Abel Alcazar Martinez',
+  'Joshua Thomas Dunn',
+  'Tyrese A. Hutcheson',
+  'Tristan Draper',
+  'Christopher Mejia',
+  'Bassam B Ali',
+  'Ronald R Cohen',
+  'Billy Edgardo Ramirez',
+  'Rodrigo Sebastian Morales',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
