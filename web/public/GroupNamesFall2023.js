32854c8d3190fde69d094137e610064eb8acb2e1
iambillmccann <bill.mccann@gmail.com>
2023-10-09 16:51:15 -0400

    Update for this year's groups

---
+++ web/public/GroupNamesFall2023.js
@@ -0,0 +1,16 @@
+const names = [
+  '...pending...',
+  'birb',
+  'Direwolves',
+  'Lowlanders',
+  'React-o-maniacs',
+  'Team 10',
+  'Fashionably Late',
+  'TeamGPT',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
