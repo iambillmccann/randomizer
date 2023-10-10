b4d7f0a63bf72bfbacb0e2cc6a198de4a1d0c671
iambillmccann <bill.mccann@gmail.com>
2023-10-10 14:42:06 -0400

    Get started on the Fall 2023 roster

---
+++ web/public/StudentNamesFall2023.js
@@ -0,0 +1,73 @@
+const names = [
+  'Joseph Antonucci',
+  'Carlos D. Bonilla',
+  'Samridhya Mukherjee',
+  'Sean A Ramirez',
+  'Palina Pauliuchenka',
+  'Sean Reiche',
+  'Marwan Youssef',
+  'Faviola Bautista',
+  'Jacob Crews',
+  'Anthony A Ruano',
+  'Mikel Abel Alcazar Martinez',
+  'Joshua Thomas Dunn',
+  'Tyrese A. Hutcheson',
+  'Tristan Draper',
+  'Christopher Mejia',
+  'Bassam B Ali',
+  'Ronald R Cohen',
+  'Billy Edgardo Ramirez',
+  'Rodrigo Sebastian Morales',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
+
+/*
+Eaton, Ryan
+Clayton, Brendan
+Kim, Sunfee
+Switlyk, Jeremy
+Wensell, Mitchell
+Palle, Kauvya
+Asral, Curtis
+Soni, Shaili
+Sugihara, Hwa-Lyang
+Vilatoro Taglianeti, Guilherme
+Ortiga, Joshua
+Le, Jonathan
+Li, Xin
+Saripella, Vinay
+Sen, Swarnali
+Vangapandu, Mythreya
+Cabrera, Jose
+Nagrajan, Gautham
+Sakunala, Hrishikesh
+Smith, Natalia
+Bolognese, Zion
+Kataryniak, Thomas
+Tobe, Yosuke
+Abounaja, Akram
+Barrera, Matthew
+Hamayama, Ryuuta
+Hegazy, Kareem
+Kapoor, Ruby
+Fnu, Abdul
+Pineda, James
+Saleem, Anbar
+Zheng, Jason
+Bunglawala, Mueen
+Bedi, Krish
+McCarten, Stephanie
+Patel, Neil
+Patel, Pruthul
+Guld, Tanner
+Chinta, Sai
+Mirandilla, Joshua
+Mogul, Burhanuddin
+Okul, Kaan
+Us, Nese
+*/
