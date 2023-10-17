175d1ec50e09793c8ac9a51d6e6f948a046fbd71
iambillmccann <bill.mccann@gmail.com>
2023-10-17 15:50:12 -0400

    Added current classes student names

--- web/public/StudentNamesFall2023.js
+++ web/public/StudentNamesFall2023.js
@@ -1,23 +1,47 @@
 const names = [
-  'Joseph Antonucci',
-  'Carlos D. Bonilla',
-  'Samridhya Mukherjee',
-  'Sean A Ramirez',
-  'Palina Pauliuchenka',
-  'Sean Reiche',
-  'Marwan Youssef',
-  'Faviola Bautista',
-  'Jacob Crews',
-  'Anthony A Ruano',
-  'Mikel Abel Alcazar Martinez',
-  'Joshua Thomas Dunn',
-  'Tyrese A. Hutcheson',
-  'Tristan Draper',
-  'Christopher Mejia',
-  'Bassam B Ali',
-  'Ronald R Cohen',
-  'Billy Edgardo Ramirez',
-  'Rodrigo Sebastian Morales',
+  'Ryan Eaton',
+  'Brendan Clayton',
+  'Sunfee Kim',
+  'Jeremy Switlyk',
+  'Mitchell Wensell',
+  'Kauvya Palle',
+  'Curtis Asral',
+  'Shaili Soni',
+  'Hwa-Lyang Sugihara',
+  'Guilherme Vilatoro Taglianeti',
+  'Joshua Ortiga',
+  'Jonathan Le',
+  'Xin Li',
+  'Vinay Saripella',
+  'Swarnali Sen',
+  'Mythreya Vangapandu',
+  'Jose Cabrera',
+  'Gautham Nagrajan',
+  'Hrishikesh Sakunala',
+  'Natalia Smith',
+  'Zion Bolognese',
+  'Thomas Kataryniak',
+  'Yosuke Tobe',
+  'Akram Abounaja',
+  'Matthew Barrera',
+  'Ryuuta Hamayama',
+  'Kareem Hegazy',
+  'Ruby Kapoor',
+  'Abdul Fnu',
+  'James Pineda',
+  'Anbar Saleem',
+  'Jason Zheng',
+  'Mueen Bunglawala',
+  'Krish Bedi',
+  'Stephanie McCarten',
+  'Neil Patel',
+  'Pruthul Patel',
+  'Tanner Guld',
+  'Sai Chinta',
+  'Joshua Mirandilla',
+  'Burhanuddin Mogul',
+  'Kaan Okul',
+  'Nese Us',
 ]
 
 const randomItems = () => {
@@ -25,49 +49,3 @@ const randomItems = () => {
 }
 
 export default randomItems
-
-/*
-Ryan Eaton
-Brendan Clayton
-Sunfee Kim
-Jeremy Switlyk
-Mitchell Wensell
-Kauvya Palle
-Curtis Asral
-Shaili Soni
-Hwa-Lyang Sugihara
-Guilherme Vilatoro Taglianeti
-Joshua Ortiga
-Jonathan Le
-Xin Li
-Vinay Saripella
-Swarnali Sen
-Mythreya Vangapandu
-Jose Cabrera
-Gautham Nagrajan
-Hrishikesh Sakunala
-Natalia Smith
-Zion Bolognese
-Thomas Kataryniak
-Yosuke Tobe
-Akram Abounaja
-Matthew Barrera
-Ryuuta Hamayama
-Kareem Hegazy
-Ruby Kapoor
-Abdul Fnu
-James Pineda
-Anbar Saleem
-Jason Zheng
-Mueen Bunglawala
-Krish Bedi
-Stephanie McCarten
-Neil Patel
-Pruthul Patel
-Tanner Guld
-Sai Chinta
-Joshua Mirandilla
-Burhanuddin Mogul
-Kaan Okul
-Nese Us
-*/
