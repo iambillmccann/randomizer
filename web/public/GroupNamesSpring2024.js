34c2b6408e26ac79a9f69f299f7505436364dbb1
Bill McCann <bill.mccann@gmail.com>
2024-02-20 14:51:25 -0500

    Update with spring 2024 teams

---
+++ web/public/GroupNamesSpring2024.js
@@ -0,0 +1,16 @@
+const names = [
+  'Ananas',
+  'Binary Brains',
+  'Developers Next Door',
+  'Goofy Goobers',
+  'Newark Hater Allegiance',
+  'React Rookies',
+  'Segmentation Cult',
+  'Bill McCann\'s Turtleneck',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
