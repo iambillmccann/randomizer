85ac7eb9629e40125e8eb93cde4cb8a4606cc2a0
Bill McCann <bill.mccann@gmail.com>
2024-10-07 15:53:16 -0400

    Add new list for CS490 Fall 2024

---
+++ web/public/GroupNamesFall2024.js
@@ -0,0 +1,23 @@
+const names = [
+  'Code Green team',
+  'Codemons',
+  'codibles',
+  'Diddy',
+  'Dream Team',
+  'ForIntI',
+  'gne-hanoj',
+  'Team Git',
+  'Outsiders',
+  'Professional Back-End Developers',
+  'TBD',
+  'Team 6',
+  'Team Aces',
+  'Team Rocket',
+  'The Pioneers',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
