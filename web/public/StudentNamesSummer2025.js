defa967aaed01a66949f1550da45552b4588a91a
Bill McCann <bill.mccann@gmail.com>
2025-06-22 20:34:38 -0400

    Latest students

---
+++ web/public/StudentNamesSummer2025.js
@@ -0,0 +1,36 @@
+const names = [
+  'Lior Biton',
+  'Paul Ellameh',
+  'Hamza Sakar',
+  'Jose Valle',
+  'Angel Cazares',
+  'Zachary Kochev',
+  'Sharushan Kumaresan',
+  'Victoria Rubio',
+  'Maahir Vohra',
+  'Mark Youssef',
+  'David Takahashi',
+  'Eidan Arriaga',
+  'Meer Modi',
+  'Chidiebube Nwaji',
+  'George Tipton',
+  'Bassil Saleh',
+  'Yuxing Deng',
+  'Bad Data',
+  'Wassem Georgey',
+  'Julian Guridi',
+  'Elias Zakarrya',
+  'Cade Bullen',
+  'Matthew Bigio',
+  'Sebastian Llaurador',
+  'Tayyab Nabeel',
+  'Mariano Ramos',
+  'Varun Sabbineni',
+  'Kimora Stowe',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
