17d22971b8658e8b82b929e33dc1cba59ec014c3
Bill McCann <bill.mccann@gmail.com>
2025-10-28 17:25:01 -0400

    Add Tuesday class

---
+++ web/public/GroupNamesFall2025Tue.js
@@ -0,0 +1,15 @@
+const names = [
+  'Team Alpha',
+  'Bad Jam',
+  'Beta Baddies',
+  'Jibbit Squad',
+  'Place Holder',
+  'Scrum and Get it',
+  'Six Eight',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
