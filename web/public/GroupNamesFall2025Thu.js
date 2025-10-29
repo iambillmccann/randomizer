2e4613e5438a74a619656e6ac7ceb07cfe218aef
Bill McCann <bill.mccann@gmail.com>
2025-10-29 19:04:38 -0400

    Change for the Thursday class

---
+++ web/public/GroupNamesFall2025Thu.js
@@ -0,0 +1,15 @@
+const names = [
+  'AANDSZ Forces',
+  'Get-er-Done',
+  'Hotsho',
+  'Ninja 33910',
+  'Nerd Herd',
+  'The Team',
+  'Wise Wombats',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
