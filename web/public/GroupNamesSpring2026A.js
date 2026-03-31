597840eb5b67a55bf683298e8054896e8985ca38
Bill McCann <bill.mccann@gmail.com>
2026-03-31 08:47:34 -0400

    Update for Spring 2026

---
+++ web/public/GroupNamesSpring2026A.js
@@ -0,0 +1,14 @@
+const names = [
+  '11:59',
+  'Banana Waffle Cookies',
+  'Lowlanders',
+  'One Piece',
+  'Stacked',
+  'Wanwan Generals',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
---
+++ web/public/GroupNamesSpring2026B.js
@@ -0,0 +1,13 @@
+const names = [
+  '4EverFiji',
+  'Dart Monkey',
+  'Null Pointers',
+  'Senjitinels',
+  'The Infinite Loops',
+]
+
+const randomItems = () => {
+  return names
+}
+
+export default randomItems
